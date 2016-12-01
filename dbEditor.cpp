891fefe8a0cf7906afb150e1adf97e3fa8025013
Supriya <supriyagadigone@hotmail.com>
2016-12-01 15:22:06 -0500

    update

--- dbEditor.cpp
+++ dbEditor.cpp
@@ -4,17 +4,36 @@
 	@author Kshamina Ghelani
 	@author Supriya Gadigone
 **/
+
 #include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
-
+extern int errno;       // error NO.
+#define MSGPERM 0600    // msg queue permission
+#define MSGTXTLEN 128   // msg text length
 
 void promptUser();
+
+key_t key; /* key to be passed to msgget() */ 
+int msgflg; /* msgflg to be passed to msgget() */ 
+int msqid; /* return value from msgget() */ 
+int msgqid;
+
 
 /**
  Main 
  **/
 int main(int argc, char* argv[])						
-{									
+{		
+	
+	  // create a message queue. If here you get a invalid msgid and use it in msgsnd() or msgrcg(), an Invalid Argument error will be returned.
+  msgqid = msgget(key, msgflg);
+  if (msgqid < 0) {
+    printf("failed to create message queue with msgqid = %d\n", msgqid);
+    return 1;
+  }
+  printf("message queue %d created\n",msgqid);				
 	
 	promptUser();
 	
@@ -34,6 +53,5 @@ void promptUser()
 	scanf("%i", &PIN);
 	printf("\nPlease enter amount of funds\n");	
 	scanf("%i", &amountOfFunds);
-		
 	
 }
--- dbServer.cpp
+++ dbServer.cpp
@@ -1,1 +1,13 @@
+/**
+	Database SErver 
+	
+	@author Kshamina Ghelani
+	@author Supriya Gadigone
+**/
+#include <stdio.h>
 
+int main(int argc, char* argv[])						
+{									
+	
+	return 0;
+}
