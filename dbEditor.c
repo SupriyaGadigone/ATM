5e129a1951e0320afc299b2df5c8c5e90d37f5f8
Kshamina <kshamina.ghelani@carleton.ca>
2016-12-08 15:39:02 -0500

    split editor and server

--- dbEditor.c
+++ dbEditor.c
@@ -36,24 +36,11 @@ void* dbServer(void *arg);
 
 int main() {
 	initialize(accounts); 
-	pthread_t server;
-	
-	pthread_create(&server, NULL, dbServer, (void*)NULL); 
-	pthread_join(server, NULL); 
+	pthread_t editor;
+	pthread_create(&editor, NULL, dbEditor, (void*)NULL);
+	pthread_join(editor, NULL);  
 	
 	return 0; 
-}
-
-void updateDatabase(char accountNumber[5], char PIN[3], char *amountOfFunds) {
-	database = fopen("dataBase.txt", "w");
-	if(database == NULL) {
-		printf("Error opening file!\n");
-		exit(1);
-	}
-	
-	fprintf(database, "%s,,%s,,%s", accountNumber, PIN, amountOfFunds); 
-	
-	fclose(database); 
 }
 
 void populateDB() {
@@ -90,47 +77,42 @@ void populateDB() {
 	fclose(database); 	
 }
 
-void* dbServer(void *arg) {
+void* dbEditor(void* arg) {
 	key_t serverKey = 1234;
-	int serverID;
-	msg message; 
-	int i;  
+	int msgflg = IPC_CREAT | 0666; // message flag
 	
-	if((serverID = msgget(serverKey, 0666))	< 0)
+	msg *message = NULL; 
+	message->mtype = 1; // Update DB message
+	int editorID;
+	
+	if((editorID = msgget(serverKey, 0666))	< 0)
 	{
-		perror("msgget for server failed");
+		perror("msgget");
 		exit(1);
 	}
 	
 	for(;;) {
-		if(msgrcv (serverID, &message, sizeof(msg), 1, 0) < 0) // receive Update DB message
-		{
-			perror("msgget");
-			exit(1);
-		}
+		char accountNumber[5] = {'\0'};
+		char PIN[3] = {'\0'};
+		char *amountOfFunds = "\0";
+
+		printf("\nPlease enter an account number (5 digits) \n");	
+		scanf("%s", accountNumber);	
 		
-		if(message.mtype == 1) {	//Update DB
-			char accountNumber[5]; 
-			char PIN[3];
-			char *amountOfFunds = {'\0'};
-
-			for(i = 0; i < 5; i++) {
-				accountNumber[i] = message.mtext[i];
-			}
-			
-			for(i = 0; i < 3; i++) {
-				PIN[i] = message.mtext[i+5];
-			}
-			
-			//Convert back to float in updateDatabase
-			for(i = 0; i < sizeof(message.mtext); i++) {
-				amountOfFunds[i] = message.mtext[i+8];
-			}
-			
-			updateDatabase(accountNumber, PIN, amountOfFunds); 
-			
-		}	
-	}
-	
+		printf("\nPlease enter your PIN number (3 digits) \n");	
+		scanf("%s", PIN);	
+		
+		printf("\nPlease enter amount of funds (precision = 2 decimals)\n");	
+		scanf("%s", amountOfFunds);
+		
+		strcpy(message->mtext, accountNumber); 
+		strcat(message->mtext, PIN);
+		strcat(message->mtext, amountOfFunds);
+		
+		//sending a message
+		if(msgsnd(editorID, message, sizeof(msg), msgflg) == -1)
+		{
+			perror ("Update DB message failed");
+		}
+	}	
 }
-
